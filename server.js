1ac3b6460243b098f8baab1ff426919ee09aff26
Antonio <amp.olivas00@gmail.com>
2021-10-26 14:38:05 -0600

    added token

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Rollbar = reqruire('rollbar')
 
 let rollbar = new Rollbar({
 
-    accessToken: '',
+    accessToken: 'dc7aa4c26c394a908012ba85fdb5a917',
     captureUncaught: true,
     catptureUnhandledRejections: true
 })
