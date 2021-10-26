c5de07ab4745ec2c54d553463ca38577f7e60a09
Antonio <amp.olivas00@gmail.com>
2021-10-26 14:48:40 -0600

    updated rollbar

--- server.js
+++ server.js
@@ -7,4 +7,24 @@ let rollbar = new Rollbar({
     accessToken: 'dc7aa4c26c394a908012ba85fdb5a917',
     captureUncaught: true,
     catptureUnhandledRejections: true
+});
+
+// rollbar.log("Hello world");
+
+const app= express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/style.css'))
 })
+
+app.use(rollbar.errorHandler())
+
+const port= process.env.PORT || 4545
+
+app.listen(port, () => console.log( `Take us to warp ${port}!`))
